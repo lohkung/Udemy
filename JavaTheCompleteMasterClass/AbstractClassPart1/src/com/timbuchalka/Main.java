e39c16cb918eaea8264285b52870d2ee18de8af3
loh <lohkung@hotmail.com>
2018-06-02 07:52:09 +0700

    add for abstract class

--- JavaTheCompleteMasterClass/AbstractClassPart1/src/com/timbuchalka/Main.java
+++ JavaTheCompleteMasterClass/AbstractClassPart1/src/com/timbuchalka/Main.java
@@ -6,6 +6,17 @@ public class Main {
 	    Dog dog = new Dog("Yorkie");
         dog.breathe();
         dog.eat();
+        
+        Bird s = new Bird("test") {
+			
+			@Override
+			public void fly() {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		s.breathe();
+		s.eat();
 
         Parrot parrot = new Parrot("Australian ringneck");
         parrot.breathe();
